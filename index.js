f6de6b8ad88d659cc8cbed32ebba0671f5598d1a
Jeremy Adams <Beoron@gmail.com>
2019-06-09 12:29:14 -0500

    Initial commit and finish exercise

---
+++ index.js
@@ -0,0 +1,21 @@
+class App extends React.Component {
+    render() {
+		return (
+		<div>
+			<h1>Slot Machines!</h1>
+			<Machine 
+				s1="🎈"
+				s2="🎈"
+				s3="🎈"
+			/>
+			<Machine 
+				s1="🎈"
+				s2="🎶"
+				s3="❤"
+			/>
+		</div>
+		)
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
